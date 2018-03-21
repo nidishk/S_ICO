74564ddd395a5338489ad965e5008366eb3ebba8
Nidish Ramakrishnan <nidishkrishnan45@gmail.com>
2018-03-21 14:32:13 +0530

    Feature: Send Transaction

---
+++ sendTransaction.js
@@ -0,0 +1,50 @@
+var StellarSdk = require('stellar-sdk');
+StellarSdk.Network.useTestNetwork();
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+var sourceKeys = StellarSdk.Keypair
+  .fromSecret('SAGDJFPOEKGG3OV644XM6KJLGIUM2K5YDE4CXYJQAKOJ4BUIPPJJU35Q');
+var destinationId = 'GD6M2KBZY3NHXKNQ2DRNG6DDFQSACYX4SXCLTCBH3CT4KBXUL5MUXVRU';
+// Transaction will hold a built transaction we can resubmit if the result is unknown.
+var transaction;
+
+// First, check to make sure that the destination account exists.
+// You could skip this, but if the account does not exist, you will be charged
+// the transaction fee when the transaction fails.
+server.loadAccount(destinationId)
+  // If the account is not found, surface a nicer error message for logging.
+  .catch(StellarSdk.NotFoundError, function (error) {
+    throw new Error('The destination account does not exist!');
+  })
+  // If there was no error, load up-to-date information on your account.
+  .then(function() {
+    return server.loadAccount("GDMFZ56F57ZYF7GGOJAQREQO4AGT5M6BKUCXY4ETNRVSCA4NBBHTQ5XR");
+  })
+  .then(function(sourceAccount) {
+    // Start building the transaction.
+    transaction = new StellarSdk.TransactionBuilder(sourceAccount)
+      .addOperation(StellarSdk.Operation.payment({
+        destination: destinationId,
+        // Because Stellar allows transaction in many currencies, you must
+        // specify the asset type. The special "native" asset represents Lumens.
+        asset: StellarSdk.Asset.native(),
+        amount: "10"
+      }))
+      // A memo allows you to add your own metadata to a transaction. It's
+      // optional and does not affect how Stellar treats the transaction.
+      .addMemo(StellarSdk.Memo.text('Test Transaction'))
+      .build();
+    // Sign the transaction to prove you are actually the person sending it.
+    transaction.sign(sourceKeys);
+    console.log(transaction);
+    // And finally, send it off to Stellar!
+    return server.submitTransaction(transaction);
+  })
+  .then(function(result) {
+    console.log('Success! Results:', result);
+  })
+  .catch(function(error) {
+    console.error('Something went wrong!', error);
+    // If the result is unknown (no response body, timeout etc.) we simply resubmit
+    // already built transaction:
+    // server.submitTransaction(transaction);
+  });
